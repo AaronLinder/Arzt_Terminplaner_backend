141d10774aca423fdc7d8ad19e8d4ffbc294ffb5
KGahan <kimgahan05@gmail.com>
2024-03-08 14:29:12 +0100

    added db connection

--- Backend/Backend/Controllers/ArztController.cs
+++ Backend/Backend/Controllers/ArztController.cs
@@ -1,4 +1,6 @@
 using Microsoft.AspNetCore.Mvc;
+using MySql.Data.MySqlClient;
+using System.Data;
 
 namespace Backend.Controllers
 {
@@ -8,21 +10,53 @@ namespace Backend.Controllers
     {
 
         private readonly ILogger<Arzt> _logger;
+        private MySqlConnection connection;
 
         public ArztController(ILogger<Arzt> logger)
         {
             _logger = logger;
+
+            string connectionString = String.Format("server={0};port={1};user id={2}; password={3}; database={4}", "10.110.49.67", 3306, "root", "mysecret", "Arzt_Terminplaner");
+
+            connection = new MySqlConnection(connectionString);
+            connection.Open();
         }
 
         [HttpGet(Name = "GetArzt")]
-        public Arzt Get()
-        {
-            return (
-                new Arzt
+        public List<Arzt> Get() {
+            MySqlCommand cmd = new MySqlCommand("SELECT * FROM Arzt");
+            cmd.CommandType = CommandType.Text;
+            cmd.Connection = connection;
+            List<Arzt> aerzteListe = new List<Arzt>();
+            string name = "Not found";
+            try
+            {
+                MySqlDataReader dr;
+                dr = cmd.ExecuteReader();
+                while (dr.Read())
                 {
-                    name = "Peter",
-                    adresse = "Blumenstraﬂe 7"
+                    aerzteListe.Add(new Arzt
+                    {
+                        name = dr.GetString("name"),
+                        adresse = dr.GetString("adresse")
+                    });
                 }
+                dr.Close();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("linder");
+            }
+            finally
+            {
+                if (connection.State == ConnectionState.Open)
+                {
+                    connection.Close();
+                }
+            }
+
+                return (
+                aerzteListe
             );          
         }
     }
